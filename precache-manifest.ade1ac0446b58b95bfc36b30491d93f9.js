7972283639b24f9ef7d3bab002feb0b0702b8ef4
Victor Espaillat <vicander_99@hotmail.com>
2020-07-18 12:53:39 -0400

    Updates

---
+++ precache-manifest.ade1ac0446b58b95bfc36b30491d93f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0540712141252451de659fe149241931",
+    "url": "/RackMath/index.html"
+  },
+  {
+    "revision": "f7bb92ff147470f5d4b0",
+    "url": "/RackMath/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1d9a239ff86312a24ac2",
+    "url": "/RackMath/static/css/main.81b54138.chunk.css"
+  },
+  {
+    "revision": "f7bb92ff147470f5d4b0",
+    "url": "/RackMath/static/js/2.a00e06f0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/RackMath/static/js/2.a00e06f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d9a239ff86312a24ac2",
+    "url": "/RackMath/static/js/main.408bfb0c.chunk.js"
+  },
+  {
+    "revision": "f9582d3191835da59a34",
+    "url": "/RackMath/static/js/runtime-main.f31abe47.js"
+  }
+]);
