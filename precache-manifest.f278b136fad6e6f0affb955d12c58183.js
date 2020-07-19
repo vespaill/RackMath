fdb2f97bc32f68f171ce4676a544aed2babe4e1d
Victor Espaillat <vicander_99@hotmail.com>
2020-07-19 12:20:52 -0400

    Updates

---
+++ precache-manifest.f278b136fad6e6f0affb955d12c58183.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fd2478f2dbdd5b3cb9960ff8bdc246f",
+    "url": "/RackMath/index.html"
+  },
+  {
+    "revision": "aefdf46437b047680c29",
+    "url": "/RackMath/static/css/2.37865b2a.chunk.css"
+  },
+  {
+    "revision": "3ecebbea096173f6466c",
+    "url": "/RackMath/static/css/main.2b401310.chunk.css"
+  },
+  {
+    "revision": "aefdf46437b047680c29",
+    "url": "/RackMath/static/js/2.a836633a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/RackMath/static/js/2.a836633a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ecebbea096173f6466c",
+    "url": "/RackMath/static/js/main.8b3267b6.chunk.js"
+  },
+  {
+    "revision": "f9582d3191835da59a34",
+    "url": "/RackMath/static/js/runtime-main.f31abe47.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.f5cea79c146ef3933168c9b71d57c113.js"
+  "/RackMath/precache-manifest.f278b136fad6e6f0affb955d12c58183.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/RackMath/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
