dd448b2e02d973aa8313de7cad275e92e62132c3
Victor Espaillat <vicander_99@hotmail.com>
2020-07-23 19:27:51 -0400

    barbell icon update

--- src/components/icons/barbell.jsx
+++ src/components/icons/barbell.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import '../../css/icons/barbell.css'
+import '../../css/icons/barbell.css';
 
 const Barbell = () => {
   return (
-    <div className="barbell">
-      <div className="barbell__shaft"></div>
-      <div className="barbell__weights">
-        <div className="barbell__weights--1">
-          <div className="weights__small"></div>
-          <div className="weights__med"></div>
-          <div className="weights__big"></div>
+    <div className="barbell-icon">
+      <div className="barbell">
+        <div className="barbell__shaft"></div>
+        <div className="plate-group--1">
+          <div className="plate-group__plate--small"></div>
+          <div className="plate-group__plate--med"></div>
+          <div className="plate-group__plate--large"></div>
         </div>
-        <div className="barbell__weights--2">
-          <div className="weights__small"></div>
-          <div className="weights__med"></div>
-          <div className="weights__big"></div>
+        <div className="plate-group--2">
+          <div className="plate-group__plate--small"></div>
+          <div className="plate-group__plate--med"></div>
+          <div className="plate-group__plate--large"></div>
         </div>
       </div>
     </div>
